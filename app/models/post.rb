cc8dd706f95b7819fa2839a9f8a83e5a11299ae1
Damilare <bestbynature2010@gmail.com>
2023-07-11 12:02:57 +0100

    :hammer: Fix linter errors

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,29 +1,29 @@
 class Post < ActiveRecord::Base
-    belongs_to :author, class_name: "User", foreign_key: "author_id"
-    has_many :comments, foreign_key: 'post_id'
-    has_many :likes, foreign_key: 'post_id'
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+  has_many :comments, foreign_key: 'post_id'
+  has_many :likes, foreign_key: 'post_id'
 
-    def update_posts_counter
-        update(posts_counter: comments.count)
-      end
+  def update_posts_counter
+    update(posts_counter: comments.count)
+  end
 
-      def update_likes_counter
-        update(likes_counter: likes.count)
-      end
+  def update_likes_counter
+    update(likes_counter: likes.count)
+  end
 
-      def self.list_all
-        Post.all.map { |post| post.title }
-      end
-    
-      def self.delete_all
-        Post.all.map { |post| post.destroy }
-      end
-    
-      def self.list_most_recent
-        Post.all.order(created_at: :desc).limit(5).map { |post| post.title }
-      end
-    
-      def recent_comments(count = 5)
-        comments.order(created_at: :desc).limit(count)
-      end
+  def self.list_all
+    Post.all.map(&:title)
+  end
+
+  def self.delete_all
+    Post.all.map(&:destroy)
+  end
+
+  def self.list_most_recent
+    Post.all.order(created_at: :desc).limit(5).map(&:title)
+  end
+
+  def recent_comments(count = 5)
+    comments.order(created_at: :desc).limit(count)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,30 +1,29 @@
 class User < ActiveRecord::Base
-    has_many :posts, foreign_key: 'author_id'
-    has_many :comments, foreign_key: 'author_id'
-    has_many :likes, foreign_key: 'author_id'
+  has_many :posts, foreign_key: 'author_id'
+  has_many :comments, foreign_key: 'author_id'
+  has_many :likes, foreign_key: 'author_id'
 
-    def recent_posts(count = 3)
-        posts.order(created_at: :desc).limit(count)
-      end
+  def recent_posts(count = 3)
+    posts.order(created_at: :desc).limit(count)
+  end
 
-      # list all available users
-      def self.list_all
-        User.all.map { |user| user.name }
-      end
+  # list all available users
+  def self.list_all
+    User.all.map(&:name)
+  end
 
-      # delete all users
-      def self.delete_all
-        User.all.map { |user| user.destroy }
-      end
+  # delete all users
+  def self.delete_all
+    User.all.map(&:destroy)
+  end
 
-      # list all users with no posts
-      def self.list_inactive
-        User.all.select { |user| user.posts.count == 0 }.map { |user| user.name }
-      end
+  # list all users with no posts
+  def self.list_inactive
+    User.all.select { |user| user.posts.count.zero? }.map(&:name)
+  end
 
-      # list all users with more than 1 post
-      def self.list_active
-        User.all.select { |user| user.posts.count > 1 }.map { |user| user.name }
-      end
-
+  # list all users with more than 1 post
+  def self.list_active
+    User.all.select { |user| user.posts.count > 1 }.map(&:name)
+  end
 end
