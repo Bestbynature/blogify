cc60b0ddb9281bf0f99d94a70e8ade67cf7eb31c
Damilare <bestbynature2010@gmail.com>
2023-07-25 08:41:35 +0100

    :zap: Add devise gem

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,13 @@
 class UsersController < ApplicationController
   before_action :set_user, only: %i[show edit update destroy]
+  before_action :authenticate_user!
 
   def index
     @users = User.all
   end
 
   def show
-    @user = User.find_by(id: params[:id])
+    # @user = User.find_by(id: params[:id])
   end
 
   def new
