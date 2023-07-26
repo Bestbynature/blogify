f49ecbedbb5d6fa97cf6a044d2aace04bcab2dd1
Damilare <bestbynature2010@gmail.com>
2023-07-26 13:38:59 +0100

    :rocket: Fix linter errors

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -1,30 +1,30 @@
 module Api
-    class CommentsController < ApplicationController
+  skip_before_action :verify_authenticity_token
+  class CommentsController < ApplicationController
+    def index
+      user = User.find(params[:user_id])
+      post = user.posts.find(params[:post_id])
+      comments = post.comments
+      render json: comments
+    end
 
-        def index
-          user = User.find(params[:user_id])
-          post = user.posts.find(params[:post_id])
-          comments = post.comments
-          render json: comments
-        end
-    
-        def create
-          user = User.find(params[:user_id])
-          post = user.posts.find(params[:post_id])
-          comment = post.comments.build(comment_params)
-          comment.user = user
-    
-          if comment.save
-            render json: comment, status: :created
-          else
-            render json: { errors: comment.errors.full_messages }, status: :unprocessable_entity
-          end
-        end
-    
-        private
-    
-        def comment_params
-          params.require(:comment).permit(:text)
-        end
+    def create
+      user = User.find(params[:user_id])
+      post = user.posts.find(params[:post_id])
+      comment = post.comments.build(comment_params)
+      comment.user = user
+
+      if comment.save
+        render json: comment, status: :created
+      else
+        render json: { errors: comment.errors.full_messages }, status: :unprocessable_entity
       end
+    end
+
+    private
+
+    def comment_params
+      params.require(:comment).permit(:text)
+    end
+  end
 end
