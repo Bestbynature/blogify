c297c3f768a556ebf8ad407aa34d734cb86d9f67
Damilare <bestbynature2010@gmail.com>
2023-07-26 13:25:50 +0100

    :zap: Add Api endpoints

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,12 @@ Rails.application.routes.draw do
       resources :comments 
     end
   end
+
+  namespace :api do
+    resources :users, only: [] do
+      resources :posts, only: [:index] do
+        resources :comments, only: [:index, :create]
+      end
+    end
+  end
 end
